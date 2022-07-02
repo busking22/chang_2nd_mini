457377d9d653f7b415af33d9fc65777c3ecd6c29
geongupark <ggus.park@gmail.com>
2022-07-02 16:35:04 +0900

    Add add_page view

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -1,3 +1,4 @@
+import 'package:chang_mini/config/colors.dart';
 import 'package:chang_mini/services/bank_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,13 +11,139 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  TextEditingController bankController = TextEditingController();
+  TextEditingController moneyController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<BankService>(
       builder: (context, service, child) {
         return GestureDetector(
           onTap: () {},
-          child: Scaffold(),
+          child: Scaffold(
+            backgroundColor: TossColor.grey1,
+            appBar: AppBar(
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.arrow_back_ios_new,
+                  color: TossColor.black,
+                ),
+              ),
+              backgroundColor: TossColor.grey1,
+              elevation: 0,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("확인"),
+                ),
+              ],
+            ),
+            body: SafeArea(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(30, 5, 30, 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "은행 추가하기",
+                      style: TextStyle(
+                        color: TossColor.black,
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text("은행명"),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: TextField(
+                            controller: bankController,
+                            textAlign: TextAlign.end,
+                            decoration: new InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        const Text("은행"),
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text("잔액"),
+                        const SizedBox(width: 8),
+                        Expanded(
+                          child: TextField(
+                            controller: moneyController,
+                            textAlign: TextAlign.end,
+                            decoration: new InputDecoration(
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        const Text("원"),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: TossColor.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          Image(
+                            image: AssetImage('assets/toss_bank.png'),
+                          ),
+                          Text(
+                            "토스 은행",
+                            style: TextStyle(
+                              color: TossColor.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "비대면으로 통장을 개설 할 수 있어요",
+                            style: TextStyle(
+                              color: TossColor.bluegrey,
+                              fontSize: 14,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              onPrimary: TossColor.white,
+                              primary: TossColor.blue,
+                            ),
+                            child: const Text("지금 바로 만들기"),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          onPrimary: TossColor.white,
+                          primary: TossColor.blue,
+                        ),
+                        child: const Text("확인"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
